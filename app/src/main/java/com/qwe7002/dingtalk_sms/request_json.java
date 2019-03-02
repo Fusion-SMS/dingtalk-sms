c24380d9657fc8d95f4fb1a29954f2e739607c7d
qwe7002 <qwe7002@hotmail.com>
2019-03-02 22:27:44 +0800

    修改request

--- app/src/main/java/com/qwe7002/dingtalk_sms/request_json.java
+++ app/src/main/java/com/qwe7002/dingtalk_sms/request_json.java
@@ -2,7 +2,7 @@ package com.qwe7002.dingtalk_sms;
 
 import com.google.gson.JsonObject;
 
-public class request_json {
-    final public String msgtype="text";
+class request_json {
+    final String msgtype="text";
     JsonObject text;
 }
